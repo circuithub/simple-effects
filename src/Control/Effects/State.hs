641849f168ab7651b6e607e439c2695f77064c5b
Luka Horvat <darwin226@gmail.com>
2016-12-10 20:06:51 +0100

    GADT approach for state

--- src/Control/Effects/State.hs
+++ src/Control/Effects/State.hs
@@ -1,63 +1,68 @@
 {-# LANGUAGE TypeFamilies, ScopedTypeVariables, FlexibleContexts, Rank2Types, ConstraintKinds #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-module Control.Effects.State (module Control.Effects.State, module Control.Effects) where
+{-# LANGUAGE MultiParamTypeClasses, GADTs #-}
+module Control.Effects.State (module Control.Effects.State, module Control.Effects1) where
 
-import Interlude
+import Interlude hiding (Set, State)
 
 import Data.IORef
 
-import Control.Effects
+import Control.Effects1
 
-data GetState s
-data SetState s
+data State s
+data Get
+data Set
+data StateMessage s a where
+    GetMessage :: StateMessage s Get
+    SetMessage :: s -> StateMessage s Set
+data StateResult s a where
+    GetResult :: { getGetResult :: s } -> StateResult s Get
+    SetResult :: StateResult s Set
 
-type instance EffectMsg (GetState s) = ()
-type instance EffectRes (GetState s) = s
+type instance EffectMsg1 (State s) = StateMessage s
+type instance EffectRes1 (State s) = StateResult s
+type instance EffectCon1 (State s) a = ()
 
-type instance EffectMsg (SetState s) = s
-type instance EffectRes (SetState s) = ()
+instance Monad m => MonadEffect1 (State s) (StateT s m) where
+    effect1 _ GetMessage = GetResult <$> get
+    effect1 _ (SetMessage s) = SetResult <$ put s
+    {-# INLINE effect1 #-}
 
-instance Monad m => MonadEffect (GetState s) (StateT s m) where
-    effect _ _ = get
+type MonadEffectState s m = MonadEffect1 (State s) m
 
-instance Monad m => MonadEffect (SetState s) (StateT s m) where
-    effect _ = put
+stateEffect :: forall s a m. MonadEffectState s m
+            => StateMessage s a -> m (StateResult s a)
+stateEffect = effect1 (Proxy :: Proxy (State s))
+{-# INLINE stateEffect #-}
 
-type MonadEffectState s m = (MonadEffect (GetState s) m, MonadEffect (SetState s) m)
-type EffectHandlerState s m = EffectHandler (GetState s) (EffectHandler (SetState s) m)
-
-getState :: forall s m. MonadEffect (GetState s) m => m s
+getState :: forall s m. MonadEffectState s m => m s
+getState = getGetResult <$> stateEffect GetMessage
 {-# INLINE getState #-}
-getState = effect (Proxy :: Proxy (GetState s)) ()
 
-setState :: forall s m. MonadEffect (SetState s) m => s -> m ()
+setState :: forall s m. MonadEffectState s m => s -> m ()
+setState s = void $ stateEffect (SetMessage s)
 {-# INLINE setState #-}
-setState = effect (Proxy :: Proxy (SetState s))
 
 modifyState :: forall s m. MonadEffectState s m => (s -> s) -> m ()
-{-# INLINE modifyState #-}
 modifyState f = do
     s <- getState
     let s' = f s in s' `seq` setState s'
+{-# INLINE modifyState #-}
 
-handleGetState :: m s -> EffectHandler (GetState s) m a -> m a
-{-# INLINE handleGetState #-}
-handleGetState = handleEffect . const
-
-handleSetState :: (s -> m ()) -> EffectHandler (SetState s) m a -> m a
-{-# INLINE handleSetState #-}
-handleSetState = handleEffect
-
-handleState :: Monad m => m s -> (s -> m ()) -> EffectHandlerState s m a -> m a
+handleState :: forall m s a. Monad m => m s -> (s -> m ())
+            -> EffectHandler1 (State s) m a -> m a
+handleState getter setter =
+    handleEffect1 handler
+    where handler :: forall b. StateMessage s b -> m (StateResult s b)
+          handler GetMessage = GetResult <$> getter
+          handler (SetMessage s) = SetResult <$ setter s
 {-# INLINE handleState #-}
-handleState getter setter = handleSetState setter . handleGetState (lift getter)
 
-handleStateIO :: MonadIO m => s -> EffectHandlerState s m a -> m a
-{-# INLINE handleStateIO #-}
+handleStateIO :: MonadIO m => s -> EffectHandler1 (State s) m a -> m a
 handleStateIO initial m = do
     ref <- liftIO (newIORef initial)
     m & handleState (liftIO (readIORef  ref)) (liftIO . writeIORef ref)
+{-# INLINE handleStateIO #-}
 
 handleStateT :: Monad m => s -> StateT s m a -> m a
-{-# INLINE handleStateT #-}
 handleStateT = flip evalStateT
+{-# INLINE handleStateT #-}
