3ec19aed43c1e237ef76555ad7ba420cca0d3336
Luka Horvat <darwin226@gmail.com>
2017-04-27 14:28:26 +0200

    Reordered type parameters for readEnv

--- src/Control/Effects/Reader.hs
+++ src/Control/Effects/Reader.hs
@@ -10,7 +10,7 @@ data ReadEnv e
 type instance EffectMsg (ReadEnv e) = ()
 type instance EffectRes (ReadEnv e) = e
 
-readEnv :: forall m e. MonadEffect (ReadEnv e) m => m e
+readEnv :: forall e m. MonadEffect (ReadEnv e) m => m e
 readEnv = effect (Proxy :: Proxy (ReadEnv e)) ()
 
 handleReadEnv :: m e -> EffectHandler (ReadEnv e) m a -> m a
