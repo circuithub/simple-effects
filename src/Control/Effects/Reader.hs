3347b83a5b91ee9be40bde523695e125de6c1295
Luka Horvat <darwin226@gmail.com>
2017-07-01 00:18:02 +0200

    Reader effect documentation

--- src/Control/Effects/Reader.hs
+++ src/Control/Effects/Reader.hs
@@ -1,5 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables, TypeFamilies, FlexibleContexts #-}
 {-# LANGUAGE DataKinds, GADTs #-}
+-- | The regular old 'MonadReader' effect with some differences. First, there's no functional
+--   dependency limiting your stack to a single environment type. This means less type inference so
+--   it might not be enough to just write 'readEnv'. Write 'readEnv @MyEnvType' instead using
+--   TypeApplications.
+--
+--   Second, the function has a less generic name and is called 'readEnv'.
+--
+--   Third, since it's a part of this effect framework, you get a 'handleReadEnv' function with
+--   which you can provide a different environment implementation _at runtime_.
 module Control.Effects.Reader (module Control.Effects.Reader, module Control.Effects) where
 
 import Control.Effects
