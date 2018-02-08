c44892363feea2d509835860a5642e3d54ffa816
Luka Horvat <darwin226@gmail.com>
2018-02-08 20:42:37 +0100

    Updated Reader to new effect API

--- src/Control/Effects/Reader.hs
+++ src/Control/Effects/Reader.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables, TypeFamilies, FlexibleContexts #-}
 {-# LANGUAGE DataKinds, GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
 -- | The regular old 'MonadReader' effect with some differences. First, there's no functional
 --   dependency limiting your stack to a single environment type. This means less type inference so
 --   it might not be enough to just write 'readEnv'. Write 'readEnv @MyEnvType' instead using
@@ -7,22 +8,21 @@
 --
 --   Second, the function has a less generic name and is called 'readEnv'.
 --
---   Third, since it's a part of this effect framework, you get a 'handleReadEnv' function with
+--   Third, since it's a part of this effect framework, you get a 'implementReadEnv' function with
 --   which you can provide a different environment implementation _at runtime_.
 module Control.Effects.Reader (module Control.Effects.Reader, module Control.Effects) where
 
 import Control.Effects
+import GHC.Generics
 
-data ReadEnv e = ReadEnv
-data instance Effect (ReadEnv e) method mr where
-    ReadEnvMsg :: Effect (ReadEnv e) 'ReadEnv 'Msg
-    ReadEnvRes :: { getReadEnvRes :: e } -> Effect (ReadEnv e) 'ReadEnv 'Res
+data ReadEnv e
+instance Effect (ReadEnv e) where
+    data EffMethods (ReadEnv e) m = ReadEnvMethods
+        { _readEnv :: m e }
+        deriving (Generic)
 
 readEnv :: forall e m. MonadEffect (ReadEnv e) m => m e
-readEnv = getReadEnvRes <$> effect ReadEnvMsg
+readEnv = _readEnv effect
 
-handleReadEnv :: Functor m => m e -> EffectHandler (ReadEnv e) m a -> m a
-handleReadEnv m = handleEffect (\ReadEnvMsg -> ReadEnvRes <$> m)
-
-handleSubreader :: MonadEffect (ReadEnv e) m => (e -> e') -> EffectHandler (ReadEnv e') m a -> m a
-handleSubreader f = handleReadEnv (f <$> readEnv)
+implementReadEnv :: Functor m => m e -> RuntimeImplementation (ReadEnv e) m a -> m a
+implementReadEnv m = implement (ReadEnvMethods m)
