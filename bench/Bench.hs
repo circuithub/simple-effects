e4bef29297dead625b205bbea361740a90063e61
Luka Horvat <darwin226@gmail.com>
2018-02-08 19:59:35 +0100

    Fixed benchmark

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,26 +1,33 @@
 {-# LANGUAGE FlexibleContexts, TypeApplications #-}
-
+{-# OPTIONS_GHC -ddump-simpl #-}
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
-import Control.Effects.State
-
+import qualified Control.Effects.State as Eff1
 import Control.Monad.State.Strict
 import Control.Monad.Trans.State.Strict (execStateT)
 
 import Criterion.Main
 
+modify'' :: MonadState s m => (s -> s) -> m ()
+modify'' f = do
+    s <- get
+    let s' = f s
+    s' `seq` put s'
+{-# INLINE modify'' #-}
+
 mtlSimple :: MonadState Int m => m ()
 mtlSimple = replicateM_ 1000000 mtlSimple'
-    where mtlSimple' = modify' (+ 1)
+    where mtlSimple' = modify'' (+ 1)
 
-effectsSimple :: (MonadEffectState Int m) => m ()
+effectsSimple :: (Eff1.MonadEffect (Eff1.State Int) m) => m ()
 effectsSimple = replicateM_ 1000000 effectsSimple'
-    where effectsSimple' = modifyState (+ (1 :: Int))
+    where effectsSimple' = Eff1.modifyState (+ (1 :: Int))
 
 main :: IO ()
 main = do
     setLocaleEncoding utf8
     defaultMain
         [ bench "MTL state" $ nfIO (execStateT mtlSimple 0)
-        , bench "Effects state" $ nfIO effState
+        , bench "Effects state" $ nfIO effState1
         ]
-    where effState = handleStateT (0 :: Int) (effectsSimple >> getState @Int)
+    where 
+    effState1 = Eff1.handleStateT (0 :: Int) (effectsSimple >> Eff1.getState @Int)
