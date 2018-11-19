65754c8b2c5aa394dd010fa9478a5129acd3adce
Luka Horvat <darwin226@gmail.com>
2018-11-19 22:30:17 +0100

    Much simpler effect deriving with better errors

--- src/Control/Effects/Generic.hs
+++ src/Control/Effects/Generic.hs
@@ -1,79 +1,63 @@
-{-# LANGUAGE ScopedTypeVariables, TypeFamilies, PolyKinds, FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables, TypeFamilies, FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances, TypeOperators, MultiParamTypeClasses #-}
 {-# LANGUAGE TypeApplications, RankNTypes, DataKinds, ViewPatterns #-}
+{-# LANGUAGE EmptyCase, FlexibleContexts, AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -fno-warn-missing-methods #-}
 module Control.Effects.Generic where
 
 import qualified GHC.Generics as Gen
 import GHC.Generics
 import Control.Monad.Trans
-import Data.Proxy
 import GHC.TypeLits
+import Control.Monad (join)
 
-data M a
 
-class (Generic (a m), Generic (a (t m)), Generic (a M)) => SimpleMethods a m t where
+class (Generic (a m), Generic (a (t m)), SimpleMethodsRep (Rep (a m)) (Rep (a (t m))) m t)
+    => SimpleMethods a (m :: * -> *) (t :: (* -> *) -> * -> *) where
     liftSimple :: a m -> a (t m)
-
-instance
-    ( Rep (a m) ~ D1 m1 (C1 m2 p)
-    , Rep (a M) ~ D1 m1 (C1 m2 pM)
-    , Rep (a (t m)) ~ D1 m1 (C1 m2 (LiftedProducts p pM m t))
-    , ProductOfSimpleMethods p pM m t
-    , Generic (a m), Generic (a (t m)), Generic (a M) )
+instance (Generic (a m), Generic (a (t m)), SimpleMethodsRep (Rep (a m)) (Rep (a (t m))) m t)
     => SimpleMethods a m t where
-    liftSimple a = case Gen.from a of
-        M1 (M1 p) -> Gen.to (M1 (M1 (liftProducts (Proxy @m) (Proxy @t) (Proxy @pM) p)))
-    {-# INLINE liftSimple #-}
+    liftSimple = Gen.to . liftSimpleRep @(Rep (a m)) @(Rep (a (t m))) @m @t . Gen.from
 
-class ProductOfSimpleMethods p pM m t where
-    type LiftedProducts p pM m t :: * -> *
-    liftProducts :: Proxy m -> Proxy t -> Proxy pM -> p x -> LiftedProducts p pM m t x
+class SimpleMethodsRep r r' (m :: * -> *) (t :: (* -> *) -> * -> *) where
+    liftSimpleRep :: r x -> r' x
+instance SimpleMethodsRep c c' m t => SimpleMethodsRep (M1 a b c) (M1 a b c') m t where
+    liftSimpleRep (M1 x) = M1 (liftSimpleRep @c @c' @m @t x)
+instance (SimpleMethodsRep a a' m t, SimpleMethodsRep b b' m t)
+    => SimpleMethodsRep (a :*: b) (a' :*: b') m t where
+    liftSimpleRep (a :*: b) = liftSimpleRep @a @a' @m @t a :*: liftSimpleRep @b @b' @m @t b
+instance (SimpleMethodsRep a a' m t, SimpleMethodsRep b b' m t)
+    => SimpleMethodsRep (a :+: b) (a' :+: b') m t where
+    liftSimpleRep (L1 a) = L1 (liftSimpleRep @a @a' @m @t a)
+    liftSimpleRep (R1 b) = R1 (liftSimpleRep @b @b' @m @t b)
+instance SimpleMethodsRep U1 U1 m t where
+    liftSimpleRep U1 = U1
+instance SimpleMethodsRep V1 V1 m t where
+    liftSimpleRep v = case v of {}
+instance SimpleMethod a a' m t
+    => SimpleMethodsRep (K1 x a) (K1 x a') m t where
+    liftSimpleRep (K1 m) = K1 (liftMethod @a @a' @m @t m)
 
-instance SimpleMethod f fM m t => ProductOfSimpleMethods (S1 m1 (Rec0 f)) (S1 m1 (Rec0 fM)) m t where
-    type LiftedProducts (S1 m1 (Rec0 f)) (S1 m1 (Rec0 fM)) m t =
-        (S1 m1 (Rec0 (LiftedMethod f fM m t)))
-    liftProducts p1 p2 _ (M1 (K1 f)) = M1 (K1 (liftMethod p1 p2 (Proxy @fM) f))
-    {-# INLINE liftProducts #-}
-instance
-    (ProductOfSimpleMethods f1 f1M m t, ProductOfSimpleMethods f2 f2M m t)
-    => ProductOfSimpleMethods (f1 :*: f2) (f1M :*: f2M) m t where
-    type LiftedProducts (f1 :*: f2) (f1M :*: f2M) m t =
-        LiftedProducts f1 f1M m t :*: LiftedProducts f2 f2M m t
-    liftProducts p1 p2 _ (f1 :*: f2) =
-        liftProducts p1 p2 (Proxy @f1M) f1 :*: liftProducts p1 p2 (Proxy @f2M) f2
-    {-# INLINE liftProducts #-}
-
-class (MonadTrans t, Monad m) => SimpleMethod f fM (m :: * -> *) (t :: (* -> *) -> * -> *) where
-    type LiftedMethod f fM m t
-    liftMethod :: Proxy m -> Proxy t -> Proxy fM -> f -> LiftedMethod f fM m t
-instance (MonadTrans t, Monad m, a ~ m x) => SimpleMethod a (M x) m t where
-    type LiftedMethod a (M x) m t = t m x
-    liftMethod _ _ _ = lift @t
-    {-# INLINE liftMethod #-}
-type family FuncRes f where
-    FuncRes (a -> b) = b
-instance (f ~ (a -> b), SimpleMethod b bM m t, IndependentOfM a m) => SimpleMethod f (a -> bM) m t where
-    type LiftedMethod f (a -> bM) m t = a -> LiftedMethod (FuncRes f) bM m t
-    liftMethod p1 p2 _ f a = liftMethod p1 p2 (Proxy @bM) (f a :: b)
-    {-# INLINE liftMethod #-}
+class SimpleMethod f ft (m :: * -> *) (t :: (* -> *) -> * -> *) where
+    liftMethod :: f -> ft
+instance SimpleMethod f ft m t => SimpleMethod (a -> f) (a -> ft) m t where
+    liftMethod f a = liftMethod @f @ft @m @t (f a)
 instance {-# OVERLAPPABLE #-}
-    ( TypeError ('Text "Effect methods must be monadic actions or functions resulting in monadic actions")
-    , Monad m, MonadTrans t )
-    => SimpleMethod a b m t
-
-class IndependentOfM (a :: k) (m :: * -> *) where
-instance
-    (IndependentOfM a m, IndependentOfM b m)
-    => IndependentOfM (a b) m
-instance
-    TypeError
-        ('Text "Parameters of methods can't depend on the monadic context ("
-        ':<>: 'ShowType m
-        ':<>: 'Text ")")
-    => IndependentOfM M m
+    ForceError (TypeError
+        ('Text "Parameters of methods can't depend on the monadic context."
+        ':$$: 'Text "The parameter `" ':<>: 'ShowType a ':<>: 'Text "` depends on `"
+        ':<>: 'ShowType m ':<>: 'Text "`"))
+    => SimpleMethod (a -> f) (a' -> ft) m t where
+    liftMethod = error "Unreachable"
+instance (MonadTrans t, Monad m) => SimpleMethod (m a) (t m a) m t where
+    liftMethod m = lift m
 instance {-# OVERLAPPABLE #-}
-    IndependentOfM (a :: k) m
+    ForceError (TypeError
+        ('Text "The result of all methods must be monadic."
+        ':$$: 'Text "One of the methods' result is of type `" ':<>: 'ShowType a
+        ':<>: 'Text "`. Maybe try `" ':<>: 'ShowType (m a) ':<>: 'Text "` instead."))
+    => SimpleMethod a b m t where
+    liftMethod = error "Unreachable"
 
 genericLiftThrough ::
     forall t e m. (MonadTrans t, Monad m, Monad (t m), SimpleMethods e m t)
@@ -83,54 +67,63 @@ genericLiftThrough = liftSimple
 
 
 
-class MonadicMethods a m where
+
+
+class (Generic (a m), MonadicMethodsRep (Rep (a m)) m, Monad m) => MonadicMethods a m where
     mergeMonadicMethods :: m (a m) -> a m
-instance
-    ( Rep (a m) ~ D1 m1 (C1 m2 p)
-    , Rep (a M) ~ D1 m1 (C1 m2 pM)
-    , ProductOfMonadicMethods p pM a m
-    , Generic (a m), Generic (a M) )
-    => MonadicMethods a m where
-    mergeMonadicMethods a = Gen.to (M1 (M1 (mergeMonadicProducts (Proxy @p) (Proxy @pM) a f)))
-        where
-        f (Gen.from -> M1 (M1 p)) = p
-    {-# INLINE mergeMonadicMethods #-}
+instance (Generic (a m), MonadicMethodsRep (Rep (a m)) m, Monad m) => MonadicMethods a m where
+    mergeMonadicMethods m = Gen.to (mergeMonadicMethodsRep @(Rep (a m)) @m (fmap Gen.from m))
 
-class ProductOfMonadicMethods p pM a m where
-    mergeMonadicProducts :: Proxy p -> Proxy pM -> m (a m) -> (a m -> p x) -> p x
-instance MonadicMethod a f fM m => ProductOfMonadicMethods (S1 m1 (Rec0 f)) (S1 m1 (Rec0 fM)) a m where
-    mergeMonadicProducts _ _ ma f = M1 (K1 (mergeMethod (Proxy @fM) (g . f) ma))
+class MonadicMethodsRep r m where
+    mergeMonadicMethodsRep :: m (r x) -> r x
+instance (MonadicMethodsRep c m, Functor m) => MonadicMethodsRep (M1 a b c) m where
+    mergeMonadicMethodsRep m = M1 (mergeMonadicMethodsRep @c @m (fmap unM1 m))
+instance (MonadicMethodsRep a m, MonadicMethodsRep b m, Functor m)
+    => MonadicMethodsRep (a :*: b) m where
+    mergeMonadicMethodsRep m =
+        mergeMonadicMethodsRep @a @m (fmap l m)
+        :*: mergeMonadicMethodsRep @b @m (fmap r m)
         where
-        g (M1 (K1 x)) = x
-    {-# INLINE mergeMonadicProducts #-}
+        l (x :*: _) = x
+        r (_ :*: x) = x
 instance
-    (ProductOfMonadicMethods f1 f1M a m, ProductOfMonadicMethods f2 f2M a m)
-    => ProductOfMonadicMethods (f1 :*: f2) (f1M :*: f2M) a m where
-    mergeMonadicProducts _ _ ma f =
-        mergeMonadicProducts (Proxy @f1) (Proxy @f1M) ma (g1 . f)
-        :*: mergeMonadicProducts (Proxy @f2) (Proxy @f2M) ma (g2 . f)
-        where
-        g1 (x :*: _) = x
-        g2 (_ :*: x) = x
-    {-# INLINE mergeMonadicProducts #-}
+    ForceError (TypeError
+        ('Text "Can't automatically derive Effect instance for an effect with multiple constructors."))
+    => MonadicMethodsRep (a :+: b) m where
+    mergeMonadicMethodsRep = error "Unreachable"
+instance MonadicMethodsRep U1 m where
+    mergeMonadicMethodsRep _ = U1
+instance
+    ForceError (TypeError
+        ('Text "Can't automatically derive Effect instance for an effect with no constructors."))
+    => MonadicMethodsRep V1 m where
+    mergeMonadicMethodsRep = error "Unreachable"
+instance (MonadicMethod a m, Functor m)
+    => MonadicMethodsRep (K1 x a) m where
+    mergeMonadicMethodsRep m = K1 (mergeMonadicMethod @a @m (fmap unK1 m))
 
-class Monad m => MonadicMethod a f fM m where
-    mergeMethod :: Proxy fM -> (a m -> f) -> m (a m) -> f
-instance (b ~ m x, Monad m) => MonadicMethod a b (M x) m where
-    mergeMethod _ f ma = do
-        a <- ma
-        f a
-    {-# INLINE mergeMethod #-}
-instance (f ~ (b -> c), Monad m, MonadicMethod a c cM m) => MonadicMethod a f (bM -> cM) m where
-    mergeMethod _ f ma b = mergeMethod (Proxy @cM) (g . f) ma
-        where
-        g = ($ b)
-    {-# INLINE mergeMethod #-}
+class MonadicMethod a m where
+    mergeMonadicMethod :: m a -> a
+instance {-# INCOHERENT #-}
+    (MonadicMethod b m, Functor m)
+    => MonadicMethod (a -> b) m where
+    mergeMonadicMethod m a = mergeMonadicMethod (fmap ($ a) m)
+instance Monad m => MonadicMethod (m a) m where
+    mergeMonadicMethod = join
 instance {-# OVERLAPPABLE #-}
-    ( TypeError ('Text "Effect methods must be monadic actions or functions resulting in monadic actions")
-    , Monad m )
-    => MonadicMethod a f fM m
+    ForceError (TypeError
+        ('Text "The result of all methods must be monadic."
+        ':$$: 'Text "One of the methods' result is of type `" ':<>: 'ShowType a
+        ':<>: 'Text "`. Maybe try `" ':<>: 'ShowType (m a) ':<>: 'Text "` instead."))
+    => MonadicMethod a m where
+    mergeMonadicMethod = error "Unreachable"
 
 genericMergeContext :: MonadicMethods a m => m (a m) -> a m
 genericMergeContext = mergeMonadicMethods
 {-# INLINE genericMergeContext #-}
+
+
+
+
+
+class ForceError (x :: *)
