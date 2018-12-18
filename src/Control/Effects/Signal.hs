e6621bb792b70596a6d0b36e217fb96ac3e5ff0f
Luka Horvat <darwin226@gmail.com>
2018-12-18 23:25:11 +0100

    Rework signal effect so it doesn't use bare transformers.

--- src/Control/Effects/Signal.hs
+++ src/Control/Effects/Signal.hs
@@ -12,7 +12,8 @@ module Control.Effects.Signal
     ( ResumeOrBreak(..), Signal(..), throwSignal, handleSignal
     , Throw, handleException, handleToEither, module Control.Effects
     , module Control.Monad.Trans.Except, MaybeT(..), discardAllExceptions, showAllExceptions
-    , HandleException(..), handleWithoutDiscarding, handleToEitherRecursive, SomeSignal
+    , HandleException(..), handleWithoutDiscarding, handleToEitherRecursive, SomeShowableSignal
+    , SomeSignal
     , signal ) where
 
 import Import hiding (liftThrough)
@@ -22,6 +23,7 @@ import GHC.TypeLits (TypeError, ErrorMessage(..))
 import Control.Effects
 import Control.Monad.Runnable
 import GHC.Generics
+import qualified Data.Text as T
 
 newtype Signal a b m = SignalMethods
     { _signal :: a -> m b }
@@ -31,7 +33,10 @@ instance Effect (Signal a b) where
 signal :: forall a b m. MonadEffect (Signal a b) m => a -> m b
 SignalMethods signal = effect
 
-newtype SomeSignal = SomeSignal { getSomeSignal :: Text } deriving (Eq, Ord, Read, Show)
+newtype SomeShowableSignal = SomeShowableSignal { getSomeShowableSignal :: Text }
+    deriving (Eq, Ord, Read, Show)
+
+data SomeSignal = SomeSignal
 
 type family UnhandledError a b :: ErrorMessage where
     UnhandledError a Void =
@@ -42,17 +47,23 @@ type family UnhandledError a b :: ErrorMessage where
            ':<>: 'TL.Text " expecting a return value of type " ':<>: 'ShowType b
      ':$$: 'TL.Text "You need to handle all the signals before running the computation"
 
-instance {-# OVERLAPPABLE #-} Monad m => MonadEffect (Signal e b) (ExceptT e m) where
-    effect = SignalMethods throwE
-instance (Show e, Monad m) => MonadEffect (Signal e b) (ExceptT SomeSignal m) where
-    effect = SignalMethods (throwE . SomeSignal . pack . show)
-instance Monad m => MonadEffect (Signal a b) (MaybeT m) where
-    effect = SignalMethods (const mzero)
 instance TypeError (UnhandledError a b) => MonadEffect (Signal a b) IO where
     effect = undefined
-instance {-# INCOHERENT #-} (Monad m, b ~ c) =>
-    MonadEffect (Signal a c) (RuntimeImplemented (Signal a b) m) where
+
+instance {-# INCOHERENT #-}
+    (Monad m, b ~ c)
+    => MonadEffect (Signal a c) (RuntimeImplemented (Signal a b) m) where
     effect = mergeContext $ RuntimeImplemented (liftThrough <$> ask)
+
+instance {-# INCOHERENT #-}
+    (Monad m, Show a)
+    => MonadEffect (Signal a b) (RuntimeImplemented (Signal SomeShowableSignal Void) m) where
+    effect = SignalMethods (fmap absurd . signal . SomeShowableSignal . T.pack . show)
+
+instance {-# INCOHERENT #-}
+    Monad m
+    => MonadEffect (Signal a b) (RuntimeImplemented (Signal SomeSignal Void) m) where
+    effect = SignalMethods (fmap absurd . signal . const SomeSignal)
 
 type Throw e = Signal e Void
 
@@ -81,10 +92,11 @@ collapseEither (Right a) = a
 --   to the caller of 'signal' and continue execution there, or do what regular exception handlers
 --   do and continue execution after the handler.
 handleSignal :: forall a b c m. Monad m
-             => (a -> m (ResumeOrBreak b c))
-             -> RuntimeImplemented (Signal a b) (ExceptT c m) c
-             -> m c
-handleSignal f = fmap collapseEither
+    => (a -> m (ResumeOrBreak b c))
+    -> RuntimeImplemented (Signal a b) (ExceptT c m) c
+    -> m c
+handleSignal f =
+    fmap collapseEither
     . runExceptT
     . implement (SignalMethods h)
     where
@@ -94,17 +106,30 @@ handleSignal f = fmap collapseEither
 
 -- | This handler can only behave like a regular exception handler. If used along with 'throwSignal'
 --   this module behaves like regular checked exceptions.
-handleException :: forall a c m. Monad m => (a -> m c) -> ExceptT a m c -> m c
-handleException f = either f return <=< runExceptT
+handleException :: forall a c m.
+    Monad m
+    => (a -> m c)
+    -> RuntimeImplemented (Signal a Void) (ExceptT a m) c
+    -> m c
+handleException f =
+    either f return
+    <=< runExceptT
+    . implement (SignalMethods throwE)
 
 -- | See documentation for 'handleException'. This handler gives you an 'Either'.
-handleToEither :: forall e a m. ExceptT e m a -> m (Either e a)
-handleToEither = runExceptT
+handleToEither ::
+    Monad m
+    => RuntimeImplemented (Signal a Void) (ExceptT a m) b
+    -> m (Either a b)
+handleToEither = handleException (return . Left) . fmap Right
 
 -- | Discard all the 'Throw' and 'Signal' effects. If any exception was thrown
 --   the result will be 'Nothing'.
-discardAllExceptions :: MaybeT m a -> m (Maybe a)
-discardAllExceptions = runMaybeT
+discardAllExceptions ::
+    Monad m
+    => RuntimeImplemented (Signal SomeException Void) (MaybeT m) a
+    -> m (Maybe a)
+discardAllExceptions = runMaybeT . implement (SignalMethods (const mzero))
 
 mapLeft :: (a -> c) -> Either a b -> Either c b
 mapLeft f (Left a) = Left (f a)
@@ -112,8 +137,14 @@ mapLeft _ (Right b) = Right b
 
 -- | Satisfies all the 'Throw' and 'Signal' constraints /if/ they all throw 'Show'able
 --   exceptions. The first thrown exception will be shown and returned as a 'Left' result.
-showAllExceptions :: Functor m => ExceptT SomeSignal m a -> m (Either Text a)
-showAllExceptions = fmap (mapLeft getSomeSignal) . runExceptT
+showAllExceptions ::
+    Monad m
+    => RuntimeImplemented (Signal SomeShowableSignal Void) (ExceptT SomeShowableSignal m) a
+    -> m (Either Text a)
+showAllExceptions =
+    fmap (mapLeft getSomeShowableSignal)
+    . runExceptT
+    . implement (SignalMethods throwE)
 
 newtype HandleException e m = HandleExceptionMethods
     { _handleWithoutDiscarding :: forall a. (e -> m a) -> m a -> m a  }
